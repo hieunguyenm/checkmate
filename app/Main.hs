acfb603e51f879c1783f310591fe30aa962e975f
Hieu Nguyen <hello@hieu.dev>
2019-10-25 13:23:55 +0100

    Add error case handling and pretty printing

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 
 import           Twitch
-import           Data.Aeson
-
 import           Data.Sort
+import           Data.Aeson
 import           GHC.Generics
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 import           Control.Concurrent.MVar.Strict
 
-import           Data.Text.Lazy                 ( Text )
+import           Data.Aeson.Encode.Pretty       ( encodePretty )
+import           Network.Mime                   ( defaultMimeLookup )
+import           Control.Monad                  ( unless )
+import           System.Directory               ( doesFileExist )
+import           System.FilePath.Posix          ( takeFileName )
 import           Data.String.Conversions        ( cs )
 import           Control.Concurrent.Async       ( concurrently_ )
 
@@ -18,10 +21,13 @@ import           Data.ByteString.Lazy          as BL
 
 import qualified Data.Bimap                    as Map
 import qualified Web.Scotty                    as Scotty
+import qualified Data.Text.Lazy                as TL
 import qualified Crypto.Hash.SHA256            as SHA256
 import qualified Data.ByteString.Base16.Lazy   as Hex
 
 type Mapping = Map.Bimap Text Text
+
+type Text = TL.Text
 
 data Pair = Pair {
   file :: Text
@@ -35,10 +41,15 @@ instance ToJSON Pair where
 
 main :: IO ()
 main = do
-  f <- BL.readFile "mappings.json"
+  f <- catch (BL.readFile "mappings.json") newEmptyJSON
   m <- newMVar $ unmarshal f
   concurrently_ (defaultMain $ "files/*" |> \s -> modifyMapping m s)
                 (Scotty.scotty 4242 $ Scotty.get "/:id" $ findFile m)
+
+newEmptyJSON :: IOError -> IO BL.ByteString
+newEmptyJSON _ = do
+  BL.writeFile "mappings.json" "[]"
+  pure "[]"
 
 modifyMapping :: MVar Mapping -> FilePath -> IO ()
 modifyMapping v s = do
@@ -46,7 +57,7 @@ modifyMapping v s = do
   let h = SHA256.hashlazy f
   let x = cs $ Hex.encode $ BL.fromStrict h :: Text
   m <- takeMVar v
-  let m' = Map.insert (cs s :: Text) x m
+  let m' = Map.insert (cs s) x m
   marshalWrite "mappings.json" m'
   putMVar v m'
 
@@ -55,16 +66,33 @@ findFile v = do
   m <- liftIO $ readMVar v
   i <- Scotty.param "id"
   f <- liftIO $ fetch Map.lookupR m i
-  Scotty.html f
+  e <- liftIO $ doesFileExist $ cs f
+  unless e $ liftIO $ cleanMapping f v
+  serveFileOrNone e f
+
+serveFileOrNone :: Bool -> TL.Text -> Scotty.ActionM ()
+serveFileOrNone True f = do
+  Scotty.file $ cs f
+  Scotty.setHeader "Content-Type" $ cs $ defaultMimeLookup $ TL.toStrict f
+  Scotty.setHeader "Content-Disposition"
+    $ TL.concat ["inline; filename=\"", cs $ takeFileName $ cs f, "\""]
+serveFileOrNone _ _ = Scotty.text "File not found"
 
 fetch :: (Text -> Mapping -> IO Text) -> Mapping -> Text -> IO Text
 fetch f m k = catch (f k m) handle
  where
   handle :: SomeException -> IO Text
-  handle _ = pure "None"
+  handle _ = pure "File not found"
+
+cleanMapping :: Text -> MVar Mapping -> IO ()
+cleanMapping p v = do
+  m <- takeMVar v
+  let m' = Map.delete (cs p) m
+  marshalWrite "mappings.json" m'
+  putMVar v m'
 
 marshalWrite :: FilePath -> Mapping -> IO ()
-marshalWrite p m = encodeFile p $ sortBy s $ Map.fold f [] m
+marshalWrite p m = BL.writeFile p $ encodePretty $ sortBy s $ Map.fold f [] m
  where
   f :: Text -> Text -> [Pair] -> [Pair]
   f a b c = Pair a b : c
@@ -78,5 +106,5 @@ unmarshal s = case decode s :: Maybe [Pair] of
   _      -> Map.empty
 
 insertAll :: [Pair] -> Mapping -> Mapping
-insertAll []       m = m
 insertAll (p : ps) m = insertAll ps $ Map.insert (file p) (hash p) m
+insertAll _        m = m
